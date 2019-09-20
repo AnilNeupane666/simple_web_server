e2003aa20a3629877d5a25adeff65b4634495be0
Anil Neupane <anil.npn1996@gmail.com>
2019-09-20 14:38:59 +0200

    routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const http = require('http');
 const server = http.createServer();
+const url =  require('url');
+
+function set_response(response, content, status=200) {
+    response.writeHead(status, {'Content-Type':'text/plain'});
+    response.write(content);
+
+}
 
 server.on('request', (request, response) => {
-   response.writeHead(200, {'Content-Type':'text/plain'});
-   response.write('Hello world');
-   response.end();
-});
+    let path = url.parse(request.url).pathname;
+    console.log(path);
 
+    if (path === '/') {
+        set_response(response, 'An Aligator approaches');
+    }else if (path === '/about'){
+        set_response(response, 'mad eby vives students  ok ');
+    }else{
+        set_response(response, 'How the f did you get here');
+    }
+
+    response.end();
+});
+ 
 server.listen(3000, () => {
   console.log('Node server created at port 3000');
 });
